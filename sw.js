bb696cdb669d835465f3341bab8a9728cdf74713
Richard <richard.anaya@gmail.com>
2016-12-15 01:25:47 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-importScripts("sw.files.js")
 importScripts("https://cdn.rawgit.com/sigiljs/trapezoid/master/trapezoid.js")
+
+var files = [
+  "index.html",
+  "todo.css",
+  "x-todo-app.html",
+  "lib/kamea.min.js",
+  "lib/sigil.min.js",
+  "lib/pouchdb.min.js",
+  "lib/webcomponents-lite.min.js"
+];
 
 var app = trapezoid();
 app.precache(files);
