d3c4d9464a79cc0427822a6090793ef38aecfe5d
Richard <richard.anaya@gmail.com>
2016-12-15 01:19:57 -0600

    making into todo

---
+++ sw.files.js
@@ -0,0 +1,9 @@
+var files = [
+  "index.html",
+  "todo.css",
+  "x-todo-app.html",
+  "lib/kamea.min.js",
+  "lib/sigil.min.js",
+  "lib/pouchdb.min.js",
+  "lib/webcomponents-lite.min.js"
+];
--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-importScripts("trapezoid.js")
+importScripts("sw.files.js")
+importScripts("https://cdn.rawgit.com/sigiljs/trapezoid/master/trapezoid.js")
 
 var app = trapezoid();
-
-app.get("/",function(req,res){
-  res.send("you made it to root! ")
-})
-
-self.addEventListener('install', function(event,options) {
-  console.log(event);
-  console.log("Installing Service Worker");
-});
-
-self.addEventListener('fetch', function(event) {
-  app.process(event);
-});
+app.precache(files);
+app.run("todo-v1");
